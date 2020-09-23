339db7cf382fbfdf897ba72566b877dae265a2de
ShrewdStylegit config --global user.name ShrewdStylegit config --global user.name ShrewdStyle <jake.webtech@gmail.com>
2020-09-23 08:08:44 +0100

    Additional content added to main app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <h1>To do list</h1>
+
+      <h3>Enter your to do's below</h3>
+      <hr />
     </div>
   );
 }
